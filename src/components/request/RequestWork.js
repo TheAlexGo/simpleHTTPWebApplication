7f59d1d62ecdd2ff35a0f77073f2f9055fdf38e1
Alexander <alex280702@mail.ru>
2021-06-14 11:49:44 +0300

    Проверка работы websocket. Рефактор кода

--- src/components/request/RequestWork.js
+++ src/components/request/RequestWork.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import FormRequest from "./FormRequest";
 import AllRequestBlock from "../AllRequestBlock";
 import Popup from "../popup/Popup";
